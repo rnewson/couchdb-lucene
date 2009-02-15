da89c207d4750ab9bf2c1177080def488b8a5f61
Robert Newson <robert.newson@gmail.com>
2009-02-15 22:18:29 +0000

    wip

--- src/test/java/org/apache/couchdb/lucene/RhinoTest.java
+++ src/test/java/org/apache/couchdb/lucene/RhinoTest.java
@@ -3,25 +3,23 @@ package org.apache.couchdb.lucene;
 import org.junit.Test;
 import org.mozilla.javascript.Context;
 import org.mozilla.javascript.ContextFactory;
+import org.mozilla.javascript.Function;
 import org.mozilla.javascript.Scriptable;
 
 public class RhinoTest {
 
 	@Test
 	public void testSimpleEval() {
-		final String indexing_function = "function(doc) { if (doc.size) { emit_int(doc.size); }  } ";
-
-		final Rhino rhino = new Rhino();
-		Object obj = rhino.evaluate(indexing_function);
-		System.err.println(obj);
+		final String source = "function(doc) { if (doc.size) {return (doc.size); }} ";
 
 		final Context ctx = new ContextFactory().enterContext();
 		final Scriptable scope = ctx.initStandardObjects();
+
+		final Function function = ctx.compileFunction(scope, source, "fun", 0, null);
+
+		final Object[] args = new Object[] { new Thing(), "b", "c" };
 		
-		final Object[] args = new Object[] { "a", "b", "c" };
-
-
-		obj = ctx.evaluateString(scope, indexing_function, "<fun>", 0, null);
+		final Object obj = function.call(ctx, scope, null, args);
 		System.err.println(obj);
 	}
 
