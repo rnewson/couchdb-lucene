0445da3926f131867fcedabdc43851adbc458444
Robert Newson <robert.newson@gmail.com>
2009-02-26 12:07:33 +0000

    wip

--- src/test/java/org/apache/couchdb/lucene/RhinoTest.java
+++ src/test/java/org/apache/couchdb/lucene/RhinoTest.java
@@ -13,13 +13,27 @@ public class RhinoTest {
 		final String source = "function(doc) { if (doc.size) {return (doc.size); }} ";
 
 		final Context ctx = new ContextFactory().enterContext();
+		ctx.setLanguageVersion(170);
 		final Scriptable scope = ctx.initStandardObjects();
 
 		final Function function = ctx.compileFunction(scope, source, "fun", 0, null);
 
 		final Object[] args = new Object[] { new Thing(), "b", "c" };
+
+		Object obj = function.call(ctx, scope, null, args);
+		System.err.println(obj);
 		
-		final Object obj = function.call(ctx, scope, null, args);
+		final String source2 = "function myobj(arg) {this.size=12}";
+		
+		final Object o = Context.jsToJava(source2, Object.class);
+		System.err.println(o);
+		System.err.println(o.getClass());
+		
+		final Object o2 = Context.javaToJS(new Thing(), scope);
+		System.err.println(o2);
+		System.err.println(o2.getClass());
+
+		obj = ctx.evaluateString(scope, source2, "fun2", 0, null);
 		System.err.println(obj);
 	}
 
