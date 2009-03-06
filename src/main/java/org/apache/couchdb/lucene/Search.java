923acf9480745190cd1ecd8a4b69e00c20a9efb0
Robert Newson <robert.newson@gmail.com>
2009-03-06 23:56:10 +0000

    clear out non-search stuff from new Search class.

--- src/main/java/org/apache/couchdb/lucene/Search.java
+++ src/main/java/org/apache/couchdb/lucene/Search.java
@@ -1,45 +1,11 @@
 package org.apache.couchdb.lucene;
 
-import static java.lang.Math.min;
-import static org.apache.couchdb.lucene.Utils.text;
-import static org.apache.couchdb.lucene.Utils.token;
-
-import java.io.File;
-import java.io.IOException;
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
-import java.util.Iterator;
 import java.util.Scanner;
-import java.util.Timer;
-import java.util.TimerTask;
 
-import net.sf.json.JSONArray;
-import net.sf.json.JSONObject;
-
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.methods.GetMethod;
-import org.apache.commons.lang.time.DurationFormatUtils;
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
-import org.apache.lucene.document.Document;
-import org.apache.lucene.document.FieldSelector;
-import org.apache.lucene.document.MapFieldSelector;
 import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.IndexWriter;
-import org.apache.lucene.index.Term;
-import org.apache.lucene.index.IndexWriter.MaxFieldLength;
-import org.apache.lucene.queryParser.ParseException;
-import org.apache.lucene.search.BooleanQuery;
-import org.apache.lucene.search.FieldDoc;
 import org.apache.lucene.search.IndexSearcher;
-import org.apache.lucene.search.Query;
-import org.apache.lucene.search.Sort;
-import org.apache.lucene.search.SortField;
-import org.apache.lucene.search.TermQuery;
-import org.apache.lucene.search.TopDocs;
-import org.apache.lucene.search.TopFieldDocs;
-import org.apache.lucene.search.BooleanClause.Occur;
-import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.NIOFSDirectory;
 
 /**
@@ -71,7 +37,7 @@ public final class Search {
 					oldReader.close();
 				}
 			}
-			
+
 			// Process search request if index exists.
 			if (searcher == null) {
 				System.out.println("{\"code\":503,\"body\":\"couchdb-lucene not available.\"}");
@@ -83,273 +49,7 @@ public final class Search {
 		}
 	}
 
-	private void openReader() throws IOException {
-		final IndexReader oldReader;
-		synchronized (mutex) {
-			oldReader = this.reader;
-		}
-
-		final IndexReader newReader;
-		if (oldReader == null) {
-			newReader = IndexReader.open(dir, true);
-		} else {
-			newReader = oldReader.reopen();
-		}
-
-		if (oldReader != newReader) {
-			synchronized (mutex) {
-				this.reader = newReader;
-				this.reader.incRef();
-				this.searcher = new IndexSearcher(this.reader);
-			}
-			if (oldReader != null) {
-				oldReader.decRef();
-			}
-		}
-	}
-
-	private static final Logger log = LogManager.getLogger(Search.class);
-
-	private static final Tika TIKA = new Tika();
-
-	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss z");
-
-	private class IndexUpdateTask extends TimerTask {
-
-		private Rhino filter = null;
-
-		@Override
-		public void run() {
-			IndexWriter writer = null;
-			boolean commit = false;
-			try {
-				final String[] dbnames = db.getAllDatabases();
-				writer = newWriter();
-				for (final String dbname : dbnames) {
-					// Database might supply a filter.
-					final JSONObject designDoc = db.getDoc(dbname, "_design/lucene", null);
-					if (designDoc.containsKey("filter")) {
-						String filterFun = designDoc.getString("filter");
-						// Strip start and end double quotes.
-						filterFun = filterFun.replaceAll("^\"*", "");
-						filterFun = filterFun.replaceAll("\"*$", "");
-						filter = new Rhino(filterFun);
-					} else {
-						filter = null;
-					}
-
-					commit |= updateDatabase(writer, dbname);
-				}
-			} catch (final IOException e) {
-				log.warn("Exception while indexing.", e);
-				commit = false;
-			} finally {
-				if (writer != null) {
-					try {
-						if (commit) {
-							writer.commit();
-							progress.save();
-							log.info("Committed updates.");
-
-							openReader();
-
-							if (reader.numDeletedDocs() >= Config.EXPUNGE_LIMIT) {
-								writer.expungeDeletes();
-							}
-							writer.close();
-						} else {
-							writer.rollback();
-						}
-					} catch (final IOException e) {
-						log.warn("Exception while committing.", e);
-					}
-				}
-			}
-		}
-
-		private boolean updateDatabase(final IndexWriter writer, final String dbname) throws HttpException, IOException {
-			final DbInfo info = db.getInfo(dbname);
-			long from = progress.getProgress(dbname);
-			long start = from;
-
-			if (from > info.getUpdateSeq()) {
-				start = from = -1;
-				progress.setProgress(dbname, -1);
-			}
-
-			if (from == -1) {
-				log.debug("index is missing or inconsistent, reindexing all documents for " + dbname);
-				writer.deleteDocuments(new Term(Config.DB, dbname));
-			}
-
-			boolean changed = false;
-			while (from < info.getUpdateSeq()) {
-				final JSONObject obj = db.getAllDocsBySeq(dbname, from, Config.BATCH_SIZE);
-				if (!obj.has("rows")) {
-					log.error("no rows found (" + obj + ").");
-					return false;
-				}
-				final JSONArray rows = obj.getJSONArray("rows");
-				for (int i = 0, max = rows.size(); i < max; i++) {
-					final JSONObject row = rows.getJSONObject(i);
-					final JSONObject value = row.optJSONObject("value");
-					final JSONObject doc = row.optJSONObject("doc");
-
-					if (doc != null) {
-						updateDocument(writer, dbname, rows.getJSONObject(i));
-						changed = true;
-					}
-					if (value != null && value.optBoolean("deleted")) {
-						writer.deleteDocuments(new Term(Config.ID, row.getString("id")));
-						changed = true;
-					}
-				}
-				from += Config.BATCH_SIZE;
-			}
-			progress.setProgress(dbname, info.getUpdateSeq());
-
-			if (changed) {
-				log.debug(String.format("%s: index caught up from %,d to %,d.", dbname, start, info.getUpdateSeq()));
-			}
-
-			return changed;
-		}
-
-		private void updateDocument(final IndexWriter writer, final String dbname, final JSONObject obj)
-				throws IOException {
-			final Document doc = new Document();
-			JSONObject json = obj.getJSONObject("doc");
-
-			// Skip design documents.
-			if (json.getString(Config.ID).startsWith("_design")) {
-				return;
-			}
-
-			// Filter, if supplied.
-			if (filter != null) {
-				json = JSONObject.fromObject(filter.parse(json.toString()));
-			}
-
-			// Standard properties.
-			doc.add(token(Config.DB, dbname, false));
-			final String id = (String) json.remove(Config.ID);
-			final String rev = (String) json.remove(Config.REV);
-
-			// Index _id and _rev as tokens.
-			doc.add(token(Config.ID, id, true));
-			doc.add(token(Config.REV, rev, true));
-
-			// Index all attributes.
-			add(doc, null, json, false);
-
-			// Attachments
-			if (json.has("_attachments")) {
-				final JSONObject attachments = json.getJSONObject("_attachments");
-				final Iterator it = attachments.keys();
-				while (it.hasNext()) {
-					final String name = (String) it.next();
-					final JSONObject att = attachments.getJSONObject(name);
-					final String url = db.url(String.format("%s/%s/%s", dbname, db.encode(id), db.encode(name)));
-					final GetMethod get = new GetMethod(url);
-					try {
-						synchronized (db) {
-							final int sc = Database.CLIENT.executeMethod(get);
-							if (sc == 200) {
-								TIKA.parse(get.getResponseBodyAsStream(), att.getString("content_type"), doc);
-							} else {
-								log.warn("Failed to retrieve attachment: " + sc);
-							}
-						}
-					} finally {
-						get.releaseConnection();
-					}
-				}
-			}
-
-			// write it
-			writer.updateDocument(new Term(Config.ID, id), doc);
-		}
-
-		private void add(final Document out, final String key, final Object value, final boolean store) {
-			if (value instanceof JSONObject) {
-				final JSONObject json = (JSONObject) value;
-				for (final Object obj : json.keySet()) {
-					add(out, (String) obj, json.get(obj), store);
-				}
-			} else if (value instanceof String) {
-				try {
-					DATE_FORMAT.parse((String) value);
-					out.add(token(key, (String) value, store));
-				} catch (final java.text.ParseException e) {
-					out.add(text(key, (String) value, store));
-				}
-			} else if (value instanceof Number) {
-				out.add(token(key, value.toString(), store));
-			} else if (value instanceof Boolean) {
-				out.add(token(key, value.toString(), store));
-			} else if (value instanceof JSONArray) {
-				final JSONArray arr = (JSONArray) value;
-				for (int i = 0, max = arr.size(); i < max; i++) {
-					add(out, key, arr.get(i), store);
-				}
-			} else if (value == null) {
-				log.warn(key + " was null.");
-			} else {
-				log.warn("Unsupported data type: " + value.getClass());
-			}
-		}
-
-	}
-
-	private static final FieldSelector FS = new MapFieldSelector(new String[] { Config.ID, Config.REV });
-
-	private final Database db = new Database(Config.DB_URL);
-
-	private final Timer timer = new Timer("IndexUpdater", true);
-
-	private final Directory dir;
-
-	private IndexReader reader;
-
-	private IndexSearcher searcher;
-
-	private final Progress progress;
-
-	private final Object mutex = new Object();
-
-	public Search() throws IOException {
-		final File f = new File(Config.INDEX_DIR);
-		dir = NIOFSDirectory.getDirectory(f);
-		if (!IndexReader.indexExists(dir)) {
-			newWriter().close();
-		}
-		this.progress = new Progress(f);
-	}
-
-	public void start() throws Exception {
-		log.info("couchdb-lucene is starting.");
-		if (IndexWriter.isLocked(dir)) {
-			log.warn("Forcibly unlocking locked index at startup.");
-			IndexWriter.unlock(dir);
-		}
-
-		Search.this.progress.load();
-		openReader();
-		// Warm the searcher.
-		query("nomatch", "dummy_field:dummy_value", null, true, 0, 5, false, false);
-
-		log.info("couchdb-lucene is started.");
-
-		timer.schedule(new IndexUpdateTask(), 0, Config.REFRESH_INTERVAL);
-	}
-
-	public void stop() throws IOException {
-		log.info("couchdb-lucene is stopping.");
-		timer.cancel();
-		this.reader.close();
-		log.info("couchdb-lucene is stopped.");
-	}
-
+	/*
 	public String query(final String dbname, final String query, final String sort_fields, final boolean ascending,
 			final int skip, final int limit, final boolean include_docs, final boolean debug) throws IOException,
 			ParseException {
@@ -511,16 +211,6 @@ public final class Search {
 
 		return result.toString();
 	}
-
-	private Query parse(final String query) throws ParseException {
-		return Config.QP.parse(query);
-	}
-
-	private IndexWriter newWriter() throws IOException {
-		final IndexWriter result = new IndexWriter(dir, Config.ANALYZER, MaxFieldLength.UNLIMITED);
-		result.setUseCompoundFile(false);
-		result.setRAMBufferSizeMB(Config.RAM_BUF);
-		return result;
-	}
+	*/
 
 }
