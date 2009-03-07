17e44e21cfc6013ce6522a9b415e1c85713c7724
Robert Newson <robert.newson@gmail.com>
2009-03-07 12:35:09 +0000

    cleanup.

--- src/main/java/org/apache/couchdb/lucene/Main.java
+++ src/main/java/org/apache/couchdb/lucene/Main.java
@@ -1,10 +1,5 @@
 package org.apache.couchdb.lucene;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-
-import net.sf.json.JSONObject;
-
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 
@@ -33,55 +28,4 @@ public final class Main {
 		return;
 	}
 
-	private static void ddd() throws Exception {
-
-		System.out.println(Utils.error("couchdb-lucene is unavailable."));
-
-		final Index index = new Index();
-		final Thread startupThread = new Thread(new Runnable() {
-
-			public void run() {
-				try {
-					index.start();
-				} catch (final Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		startupThread.start();
-
-		final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in, "UTF-8"));
-		String line = null;
-
-		// Promptly returns errors until started.
-		while (startupThread.isAlive() && (line = reader.readLine()) != null) {
-			System.out.println(Utils.error("couchdb-lucene is unavailable."));
-		}
-
-		// Main processing loop.
-		while ((line = reader.readLine()) != null) {
-			try {
-				// Parse query.
-				final JSONObject obj = JSONObject.fromObject(line);
-
-				final String db = obj.getJSONObject("info").getString("db_name");
-				final JSONObject query = obj.getJSONObject("query");
-				final String q = query.getString("q");
-				final String sort = query.optString("sort", null);
-				final int skip = query.optInt("skip", 0);
-				final int limit = query.optInt("limit", 25);
-				final boolean debug = query.optBoolean("debug", false);
-				final boolean asc = query.optBoolean("asc", true);
-				final boolean include_docs = query.optBoolean("include_docs", false);
-
-				// Execute query.
-				System.out.println(index.query(db, q, sort, asc, skip, limit, include_docs, debug));
-			} catch (final Exception e) {
-				log.warn("Exception in main loop (line=\"" + line + "\")", e);
-				System.out.println(Utils.throwableToJSON(e));
-			}
-		}
-
-		index.stop();
-	}
 }
--- src/main/java/org/apache/couchdb/lucene/Search.java
+++ src/main/java/org/apache/couchdb/lucene/Search.java
@@ -2,8 +2,6 @@ package org.apache.couchdb.lucene;
 
 import java.util.Scanner;
 
-import org.apache.log4j.LogManager;
-import org.apache.log4j.Logger;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.NIOFSDirectory;
@@ -12,8 +10,6 @@ import org.apache.lucene.store.NIOFSDirectory;
  * Search entry point.
  */
 public final class Search {
-
-	private static final Logger logger = LogManager.getLogger(Search.class);
 
 	public static void main(final String[] args) throws Exception {
 		IndexReader reader = null;
@@ -39,7 +35,7 @@ public final class Search {
 			}
 
 			final String line = scanner.nextLine();
-			
+
 			// Process search request if index exists.
 			if (searcher == null) {
 				System.out.println("{\"code\":503,\"body\":\"couchdb-lucene not available.\"}");
@@ -54,94 +50,5 @@ public final class Search {
 			reader.close();
 		}
 	}
-
-	/*
-	 * public String query(final String dbname, final String query, final String
-	 * sort_fields, final boolean ascending, final int skip, final int limit,
-	 * final boolean include_docs, final boolean debug) throws IOException,
-	 * ParseException { if (limit > Config.MAX_LIMIT) { return
-	 * Utils.error("limit of " + limit + " exceeds maximum limit of " +
-	 * Config.MAX_LIMIT); }
-	 * 
-	 * final BooleanQuery bq = new BooleanQuery(); bq.add(new TermQuery(new
-	 * Term(Config.DB, dbname)), Occur.MUST); bq.add(parse(query), Occur.MUST);
-	 * 
-	 * final IndexSearcher searcher; synchronized (mutex) { searcher =
-	 * this.searcher; }
-	 * 
-	 * searcher.getIndexReader().incRef(); final TopDocs td; final long start =
-	 * System.nanoTime(); try { if (sort_fields == null) { td =
-	 * searcher.search(bq, null, skip + limit); } else { final Sort sort; if
-	 * (sort_fields.indexOf(",") != -1) { sort = new
-	 * Sort(sort_fields.split(",")); } else { sort = new Sort(sort_fields,
-	 * !ascending); } td = searcher.search(bq, null, skip + limit, sort); } }
-	 * finally { searcher.getIndexReader().decRef(); } final long
-	 * search_duration = System.nanoTime() - start;
-	 * 
-	 * TopFieldDocs tfd = null; if (td instanceof TopFieldDocs) { tfd =
-	 * (TopFieldDocs) td; }
-	 * 
-	 * final JSONObject json = new JSONObject(); json.element("total_rows",
-	 * td.totalHits); // Report on sorting order, if specified. if (tfd != null)
-	 * { final JSONArray sort_order = new JSONArray(); for (final SortField
-	 * field : tfd.fields) { final JSONObject col = new JSONObject();
-	 * col.element("field", field.getField()); col.element("reverse",
-	 * field.getReverse());
-	 * 
-	 * final String type; switch (field.getType()) { case SortField.DOC: type =
-	 * "doc"; break; case SortField.SCORE: type = "score"; break; case
-	 * SortField.INT: type = "int"; break; case SortField.LONG: type = "long";
-	 * break; case SortField.BYTE: type = "byte"; break; case SortField.CUSTOM:
-	 * type = "custom"; break; case SortField.DOUBLE: type = "double"; break;
-	 * case SortField.FLOAT: type = "float"; break; case SortField.SHORT: type =
-	 * "short"; break; case SortField.STRING: type = "string"; break; default:
-	 * type = "unknown"; break; } col.element("type", type);
-	 * sort_order.add(col); } json.element("sort_order", sort_order); }
-	 * 
-	 * final int max = min(td.totalHits, limit); final String[] fetch_ids =
-	 * include_docs ? new String[max] : null; final JSONArray rows = new
-	 * JSONArray(); for (int i = skip; i < skip + max; i++) { final Document doc
-	 * = searcher.doc(td.scoreDocs[i].doc, FS); final JSONObject obj = new
-	 * JSONObject(); obj.element("_id", doc.get(Config.ID)); obj.element("_rev",
-	 * doc.get(Config.REV)); obj.element("score", td.scoreDocs[i].score); if
-	 * (tfd != null) { final FieldDoc fd = (FieldDoc) tfd.scoreDocs[i];
-	 * obj.element("sort_order", fd.fields); } if (fetch_ids != null) {
-	 * fetch_ids[i - skip] = obj.getString(Config.ID); } if (include_docs) {
-	 * obj.element("doc", db.getDoc(dbname, obj.getString("_id"),
-	 * obj.getString("_rev"))); } rows.add(obj); }
-	 * 
-	 * if (fetch_ids != null) { final JSONObject fetch_docs = db.getDocs(dbname,
-	 * fetch_ids); final JSONArray arr = fetch_docs.getJSONArray("rows"); for
-	 * (int i = 0; i < max; i++) { rows.getJSONObject(i).element("doc",
-	 * arr.getJSONObject(i).getJSONObject("doc")); } }
-	 * 
-	 * json.element("rows", rows); final long total_duration = System.nanoTime()
-	 * - start;
-	 * 
-	 * final JSONObject result = new JSONObject(); result.element("code", 200);
-	 * 
-	 * if (debug) { final StringBuilder builder = new StringBuilder(500); //
-	 * build basic lines. builder.append("<dl>");
-	 * builder.append("<dt>database name</dt><dd>" + dbname + "</dd>");
-	 * builder.append("<dt>query</dt><dd>" + bq + "</dd>");
-	 * builder.append("<dt>sort</dt><dd>" + sort_fields + "</dd>");
-	 * builder.append("<dt>skip</dt><dd>" + skip + "</dd>");
-	 * builder.append("<dt>limit</dt><dd>" + limit + "</dd>");
-	 * builder.append("<dt>total_rows</dt><dd>" + json.getInt("total_rows") +
-	 * "</dd>"); if (json.get("sort_order") != null) {
-	 * builder.append("<dt>sort_order</dt><dd>" + json.get("sort_order") +
-	 * "</dd>"); } builder.append("<dt>search duration</dt><dd>" +
-	 * DurationFormatUtils.formatDurationHMS(search_duration / 1000000) +
-	 * "</dd>"); builder.append("<dt>total duration</dt><dd>" +
-	 * DurationFormatUtils.formatDurationHMS(total_duration / 1000000) +
-	 * "</dd>"); builder.append("<dt>rows</dt><dd>");
-	 * builder.append("<ol start=\"" + skip + "\">"); for (int i = 0; i <
-	 * rows.size(); i++) { builder.append("<li>" + rows.get(i) + "</li>"); }
-	 * builder.append("</ol>"); builder.append("</dd>");
-	 * builder.append("</dl>"); result.element("body", builder.toString()); }
-	 * else { result.element("json", json); }
-	 * 
-	 * return result.toString(); }
-	 */
 
 }
