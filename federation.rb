8a558ed46701b5230a21d837051751bdb563b533
Robert Newson <robert.newson@gmail.com>
2009-03-11 15:57:40 +0000

    improved federation script.

--- federation.rb
+++ federation.rb
@@ -3,39 +3,34 @@
 require 'net/http'
 require 'json'
 
-servers = %w(localhost localhost localhost localhost)
-total_rows = 0
-page_size = 0
-top_rows = []
+servers = %w(localhost localhost)
+result = {}
+
 threads=[]
 for server in servers
   threads << Thread.new(server) do |url|
     h = Net::HTTP.new(url, 5984)
-    resp = h.get('/enron/_fti?q=content:enron&sort=/_id&skip=10')
+    resp = h.get('/' + ARGV[0] + '/_fti?' + ARGV[1])
     json = JSON.parse(resp.body)
-    
-    # Accumulate federated result
-    page_size = json['limit']
 
-    total_rows = total_rows + json['total_rows']
-    for row in json['rows']
-      # remember origin server.
-      row['server'] = server
-      # Add it.
-      top_rows << row
-    end    
-    # Sort the partial result
-    if json.has_key?('sort_order')
-      top_rows.sort!{|a,b| a['sort_order'] <=> b['sort_order']}
+    if (!result.has_key?('q')) then
+      result = json
     else
-      top_rows.sort!{|a,b| a['_id'] <=> b['_id']}
+      # Accounting
+      result['total_rows'] += json['total_rows']
+      # Merge in new rows
+      result['rows'].concat json['rows']      
+      if json.has_key?('sort_order')
+        result['rows'].sort!{|a,b| a['sort_order'] <=> b['sort_order']}
+      else
+        result['rows'].sort!{|a,b| a['_id'] <=> b['_id']}
+      end
+      # Drop extraneous rows.
+      result['rows'].slice!(result['limit']..result['rows'].size)      
     end
-    # Drop extraneous hits.
-    top_rows.slice!(page_size..top_rows.size)
   end
 end
 # Wait for all responses
 threads.each{|thr| thr.join}
 
-result =  JSON.generate ['total_rows' => total_rows, 'rows' => top_rows]
-puts result
+puts JSON.generate result
