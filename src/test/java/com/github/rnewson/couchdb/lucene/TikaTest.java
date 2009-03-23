dedb5886d3f84eda5011aef84e8215cfb817af97
Robert Newson <robert.newson@gmail.com>
2009-03-23 23:25:42 +0000

    remove useless TikaTests

--- src/test/java/com/github/rnewson/couchdb/lucene/TikaTest.java
+++ src/test/java/com/github/rnewson/couchdb/lucene/TikaTest.java
@@ -16,21 +16,16 @@ package com.github.rnewson.couchdb.lucene;
  * limitations under the License.
  */
 
-import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.not;
 import static org.hamcrest.Matchers.nullValue;
 import static org.junit.Assert.assertThat;
 
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
 import org.apache.lucene.document.Document;
 import org.junit.Before;
 import org.junit.Test;
-
-import com.ibm.icu.text.CharsetDetector;
-import com.ibm.icu.text.CharsetMatch;
 
 public class TikaTest {
 
@@ -53,32 +48,6 @@ public class TikaTest {
 	public void testXML() throws IOException {
 		parse("example.xml", "text/xml");
 		assertThat(doc.getField(Config.BODY), not(nullValue()));
-	}
-
-	@Test
-	public void testEnglish() throws IOException {
-		assertThat(detectLanguage("english"), is(nullValue()));
-		assertThat(detectLanguage("english text here"), is("en"));
-	}
-
-	@Test
-	public void testGerman() throws IOException {
-		assertThat(detectLanguage("Alle Menschen sind frei und gleich"), is("de"));
-	}
-
-	@Test
-	public void testFrench() throws IOException {
-		assertThat(detectLanguage("Me permettez-vous, dans ma gratitude"), is("fr"));
-	}
-
-	private String detectLanguage(final String in) throws IOException {
-		CharsetDetector detector = new CharsetDetector();
-		detector.setText(new ByteArrayInputStream(in.getBytes()));
-		CharsetMatch match = detector.detect();
-		if (match.getConfidence() < 50) {
-			return null;
-		}
-		return match.getLanguage();
 	}
 
 	private void parse(final String resource, final String type) throws IOException {
