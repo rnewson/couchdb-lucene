9b0e57f8caccb8dee04fbfff426dc39fb0f77047
Robert Newson <robert.newson@gmail.com>
2009-09-02 10:49:54 +0100

    placeholder servlets for standalone mode.

---
+++ src/main/java/com/github/rnewson/couchdb/lucene/servlet/IndexingServlet.java
@@ -0,0 +1,21 @@
+package com.github.rnewson.couchdb.lucene.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public final class IndexingServlet extends HttpServlet {
+
+    private static final long serialVersionUID = 1L;
+
+    @Override
+    protected void doPost(final HttpServletRequest req, final HttpServletResponse resp) throws ServletException,
+            IOException {
+        // TODO Auto-generated method stub
+        super.doPost(req, resp);
+    }
+
+}
