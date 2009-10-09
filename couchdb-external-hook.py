efc4e383ff1755c4213d3a435b56214b530d2400
Robert Newson <robert.newson@gmail.com>
2009-10-09 01:19:44 +0100

    fix etag caching.

--- couchdb-external-hook.py
+++ couchdb-external-hook.py
@@ -3,6 +3,8 @@
 import sys
 import urllib
 
+from urllib2 import Request, urlopen, HTTPError
+
 host = "localhost"
 port = 5985
 
@@ -21,11 +23,18 @@ def requests():
         line = sys.stdin.readline()
 
 def respond(req):
-    f = urllib.urlopen("http://%s:%s/search/%s/%s/%s?%s" % (host, port,
-                                                            req["path"][0], req["path"][2], req["path"][3],
-                                                            urllib.urlencode(req["query"])))
-    body = f.read()
-    sys.stdout.write("%s\n" % json.dumps({"code":f.getcode(),"body":body}))
+    hreq = Request("http://%s:%s/search/%s/%s/%s?%s" % (host, port,
+                                                                req["path"][0], req["path"][2], req["path"][3],
+                                                                urllib.urlencode(req["query"])))
+    if "If-None-Match" in req["headers"]:
+        hreq.add_header("If-None-Match", req["headers"]["If-None-Match"])
+    try:
+        f = urlopen(hreq)
+        body = f.read()
+        headers = f.info().dict
+        sys.stdout.write("%s\n" % json.dumps({"code":f.getcode(),"headers":headers,"body":body}))
+    except HTTPError, e:
+        sys.stdout.write("%s\n" % json.dumps({"code":e.code}))
     sys.stdout.flush()
 
 def main():
