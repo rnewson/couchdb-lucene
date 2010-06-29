d957e64409599e6ddc18f741168e259bf339f3ca
Robert Newson <robert.newson@gmail.com>
2010-06-30 00:14:11 +0100

    make Welcome request report actual version number from manifest.

--- src/main/java/com/github/rnewson/couchdb/lucene/LuceneServlet.java
+++ src/main/java/com/github/rnewson/couchdb/lucene/LuceneServlet.java
@@ -148,9 +148,10 @@ public final class LuceneServlet extends HttpServlet {
 	private void handleWelcomeReq(final HttpServletRequest req,
 			final HttpServletResponse resp) throws ServletException,
 			IOException {
+	    final Package p = this.getClass().getPackage();
 		final JSONObject welcome = new JSONObject();
 		welcome.put("couchdb-lucene", "Welcome");
-		welcome.put("version", "0.5.0");
+		welcome.put("version", p.getImplementationVersion());
 		ServletUtils.writeJSON(resp, welcome);
 	}
 
