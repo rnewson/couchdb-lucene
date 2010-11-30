b6a75d77d5dc343412bfbad0eca4e0b09117f450
Robert Newson <robert.newson@gmail.com>
2010-11-30 18:39:11 +0000

    add some analyzer tests.

--- src/test/java/com/github/rnewson/couchdb/lucene/util/AnalyzersTest.java
+++ src/test/java/com/github/rnewson/couchdb/lucene/util/AnalyzersTest.java
@@ -4,12 +4,17 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertThat;
 
+import java.io.StringReader;
+import java.util.Iterator;
+
 import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.PerFieldAnalyzerWrapper;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.lucene.analysis.WhitespaceAnalyzer;
 import org.apache.lucene.analysis.fr.FrenchAnalyzer;
 import org.apache.lucene.analysis.snowball.SnowballAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.analysis.tokenattributes.TermAttribute;
 import org.junit.Test;
 
 public class AnalyzersTest {
@@ -48,11 +53,23 @@ public class AnalyzersTest {
     @Test
     public void testPorter() throws Exception {
         assertThat(Analyzers.getAnalyzer("porter"), is(PorterStemAnalyzer.class));
+        assertAnalysisOf(Analyzers.getAnalyzer("porter"), "foo bar", "foo", "bar");
     }
     
     @Test
     public void testSnowball() throws Exception {
         assertThat(Analyzers.getAnalyzer("snowball"), is(SnowballAnalyzer.class));
+        assertAnalysisOf(Analyzers.getAnalyzer("snowball"), "foo bar", "foo", "bar");
+    }
+
+    private void assertAnalysisOf(final Analyzer analyzer, final String input, final String... expected) throws Exception {
+        final TokenStream stream = analyzer.tokenStream("foo", new StringReader(input));
+        int i=0;
+        while (stream.incrementToken()) {
+            final TermAttribute termAttribute = stream.getAttribute(TermAttribute.class);
+            assertThat(termAttribute.term(), is(expected[i++]));
+        }
+        stream.close();
     }
 
 }
