89887ffae9a9c43dd8e038c58ff7116c457eded3
Robert Newson <robert.newson@gmail.com>
2011-04-11 00:11:42 +0100

    async-http-client spike

--- src/main/java/com/github/rnewson/couchdb/lucene/Ning.java
+++ src/main/java/com/github/rnewson/couchdb/lucene/Ning.java
@@ -1,13 +1,17 @@
 package com.github.rnewson.couchdb.lucene;
 
-import java.util.concurrent.Future;
+import org.json.JSONObject;
 
 import com.ning.http.client.AsyncHandler;
 import com.ning.http.client.AsyncHttpClient;
+import com.ning.http.client.AsyncHttpClientConfig;
+import com.ning.http.client.AsyncHttpClientConfig.Builder;
+import com.ning.http.client.AsyncHttpProvider;
 import com.ning.http.client.HttpResponseBodyPart;
 import com.ning.http.client.HttpResponseHeaders;
 import com.ning.http.client.HttpResponseStatus;
 import com.ning.http.client.Response;
+import com.ning.http.client.providers.jdk.JDKAsyncHttpProvider;
 
 public class Ning {
 
@@ -20,8 +24,23 @@ public class Ning {
 		}
 
 		public STATE onBodyPartReceived(final HttpResponseBodyPart part) throws Exception {
-			System.out.print(url + ": " + new String(part.getBodyPartBytes()));
-			return STATE.CONTINUE;
+			final String line = new String(part.getBodyPartBytes(), "UTF-8");
+			if ("\n".equals(line)) {
+				System.err.println(url + ": heartbeat");
+				return STATE.CONTINUE;
+			} else {
+				final JSONObject json = new JSONObject(line);
+				if (json.has("last_seq")) {
+					System.err.println(url + ": response ended at sequence " + json.getLong("last_seq"));
+					return STATE.ABORT;
+				}
+				if (json.has("error")) {
+					System.err.println(url + ": response encountered error: " + json.getString("reason"));
+					return STATE.ABORT;
+				}
+				System.out.println(url + ": " + json);
+				return STATE.CONTINUE;
+			}
 		}
 
 		public Response onCompleted() throws Exception {
@@ -39,6 +58,7 @@ public class Ning {
 		}
 
 		public void onThrowable(final Throwable t) {
+			System.err.println(url + " threw;");
 			t.printStackTrace();
 		}
 
@@ -47,7 +67,7 @@ public class Ning {
 	public static void main(final String[] args) throws Exception {
 		AsyncHttpClient asyncHttpClient = new AsyncHttpClient();
 		for (int i = 0; i < args.length; i++) {
-			Future<Response> f = asyncHttpClient.prepareGet(args[i] + "/_changes?feed=continuous&heartbeat=60000").execute(
+			asyncHttpClient.prepareGet(args[i] + "/_changes?feed=continuous&heartbeat=65000").execute(
 					new MyHandler(args[i]));
 		}
 	}
