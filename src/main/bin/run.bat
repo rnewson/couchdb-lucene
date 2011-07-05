16b5318ac720c2852f45406f46d9b05b2fd2d372
Henrik <henrik@newdawn.dk>
2011-07-05 12:58:09 +0200

    Added run.bat - for running on windows

---
+++ src/main/bin/run.bat
@@ -0,0 +1,22 @@
+@ECHO OFF
+SETLOCAL EnableDelayedExpansion
+SET JAVA_OPTS=-server -Xmx1g
+SET CLASS=com.github.rnewson.couchdb.lucene.Main
+SET CLASSPATH=conf
+
+FOR %%G IN (lib/*.jar) DO (
+	CALL :addclasspath lib/%%G
+)
+GOTO :done
+
+:addclasspath
+SET CLASSPATH=%CLASSPATH%:%1
+GOTO :eof
+
+:done
+
+ECHO CLASSPATH=%CLASSPATH%
+ 
+java %JAVA_OPTS% -cp %CLASSPATH% %CLASS%
+
+ENDLOCAL
