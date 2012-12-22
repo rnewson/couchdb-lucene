c580034534b6e86afbc198e00eb0d3b00175d7d2
Robert Newson <robert.newson@cloudant.com>
2012-12-22 21:33:56 +0000

    Update copyright notices

--- src/main/java/com/github/rnewson/couchdb/lucene/util/Utils.java
+++ src/main/java/com/github/rnewson/couchdb/lucene/util/Utils.java
@@ -1,13 +1,11 @@
-package com.github.rnewson.couchdb.lucene.util;
-
-/**
- * Copyright 2009 Robert Newson
- * 
- * Licensed under the Apache License, Version 2.0 (the "License"); 
- * you may not use this file except in compliance with the License. 
+/*
+ * Copyright Robert Newson
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0 
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -15,6 +13,8 @@ package com.github.rnewson.couchdb.lucene.util;
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package com.github.rnewson.couchdb.lucene.util;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
