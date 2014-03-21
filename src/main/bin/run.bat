7917dd5bc6a6158b310ac8340013be2b52e70df5
Robert Newson <robert.newson@gmail.com>
2014-03-21 19:29:54 +0000

    Use short filenames on Windows becauses spaces are evil

--- src/main/bin/run.bat
+++ src/main/bin/run.bat
@@ -5,6 +5,6 @@ cd %~dp0..
 SET JAVA_OPTS= -Xmx1g
 SET CLASS=com.github.rnewson.couchdb.lucene.Main
 SET CLASSPATH="conf"
-for %%i in ("lib\*.jar") do @SET CLASSPATH=!CLASSPATH!;"%%~dpfi"
+for %%i in ("lib\*.jar") do @SET CLASSPATH=!CLASSPATH!;"%%~sdpfi"
 java %JAVA_OPTS% -cp %CLASSPATH% %CLASS% > nul
 cd %OLDCD%
